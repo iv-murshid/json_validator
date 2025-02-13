be9e948edfb293353182c93278966796a2697394
iv-murshid <murshid.raja@instaview.ai>
2025-02-13 17:35:17 +0530

    refactor JSON validation and add time format check

--- main.go
+++ main.go
@@ -18,10 +18,7 @@ func main() {
 	}`
 
 	// JSON Data as a string
-	jsonStr := `{
-		"name": "John Doe",
-		"age": 25
-	}`
+	jsonStr := `{"name": "John Doe","age": 25}`
 
 	// Load schema and JSON data as loaders
 	schemaLoader := gojsonschema.NewStringLoader(schemaStr)
@@ -30,7 +27,8 @@ func main() {
 	// Validate JSON against schema
 	result, err := gojsonschema.Validate(schemaLoader, jsonLoader)
 	if err != nil {
-		return 
+		fmt.Println(err)
+		return
 	}
 
 	fmt.Println(result)
@@ -44,4 +42,45 @@ func main() {
 			fmt.Println("-", desc)
 		}
 	}
+
+	// check for regex expression
+	schema2 := `{
+		"type": "string",
+		"pattern": "^(?:[01]\\d|2[0-3]):[0-5]\\d$"
+	}`
+
+	time := "11:11"
+
+	// Load schema and JSON data as loaders
+	s := gojsonschema.NewStringLoader(schema2)
+	// for string we need to have string of for `"value"`
+	// for schema to understand that this is of type string
+	j := gojsonschema.NewStringLoader(fmt.Sprintf(`"%s"`, time)) 
+
+	// Validate JSON against schema
+	r, err := gojsonschema.Validate(s, j)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(r)
+
+	// Check validation result
+	if r.Valid() {
+		fmt.Println("JSON is valid!")
+	} else {
+		fmt.Println("JSON is invalid. Errors:")
+		for _, desc := range r.Errors() {
+			fmt.Println("-", desc)
+		}
+	}
+
+	// solution 1
+	// - create different attribute type and validate it accordingly
+	// solution 2
+	// - using type decide whether to use string or json validator
+	// solution 3
+	// - use only json to store data , remove strings
+
 }
